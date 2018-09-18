958e4bf4b7c2e8623f4cf57459c67d99dea5cc9f
Emma S <steinm_e1@denison.edu>
2018-09-18 10:07:30 -0400

    HTML practice

---
+++ test.py
@@ -0,0 +1,11 @@
+#/Library/Frameworks/Python.framework/Versions/3.5/bin/python3
+import time 
+
+
+def idk(alert):
+	begin = time.time()
+	while(time.time() - begin)<2:
+		print(alert)
+	
+
+idk("hi")
